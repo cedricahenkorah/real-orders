0f1ed37ee648cea0f6c2d61c2313d4c47f599068
cedricahenkorah <cedricahenkorah@gmail.com>
2024-11-24 16:19:25 +0000

    refactor: used a primary constructor

--- src/OrderAPI/Services/OrderService.cs
+++ src/OrderAPI/Services/OrderService.cs
@@ -4,14 +4,9 @@ using OrderAPI.Repositories;
 
 namespace OrderAPI.Services
 {
-    public class OrderService : IOrderService
+    public class OrderService(IOrderRepository orderRepository) : IOrderService
     {
-        private readonly IOrderRepository _orderRepository;
-
-        public OrderService(IOrderRepository orderRepository)
-        {
-            _orderRepository = orderRepository;
-        }
+        private readonly IOrderRepository _orderRepository = orderRepository;
 
         public async Task<Order> CreateOrderAsync(CreateOrderRequestDto createOrderDto)
         {
